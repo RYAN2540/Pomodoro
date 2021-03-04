a0bba1b58c2212c6ed55c61e680871e94e109602
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-04 13:44:56 +0300

    Merge tree

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from app import db
+from datetime import datetime
+class Todos(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    category = db.Column(db.String(50), nullable = True)
+    description = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean(), default=False)
+    create_date = db.Column(db.DateTime(), default=datetime.now())
+
+class Timer(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    pomodoro_interval = db.Column(db.Integer())
+    break_interval = db.Column(db.Integer())
+
+class Feedbacks(db.Model):
+	id = db.Column(db.Integer(), primary_key=True)
+	username = db.Column(db.String(50))
+	feedback = db.Column(db.String(100))
