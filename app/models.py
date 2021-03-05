d8810875faed5433a7cd58678a955e05382abe64
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-05 14:08:19 +0300

    Add test for user amd feedback class

--- app/models.py
+++ app/models.py
@@ -64,4 +64,3 @@ class Feedbacks(db.Model):
 	id = db.Column(db.Integer(), primary_key=True)
 	username = db.Column(db.String(50))
 	feedback = db.Column(db.String(100))
-
---
+++ app/tests/test_feedback.py
@@ -0,0 +1,25 @@
+import unittest
+from app.models import Feedbacks
+
+class TestFeedback(unittest.TestCase):
+    '''
+    Test class to class the behaviour of the Feedback class
+    '''
+    def setUp(self):
+        self.user_John_Doe = User(username = 'John Doe', password = 'Iamjohndoe', email = 'johndoe@gmail.com', bio = 'I love coding',
+        profile_pi_path = 'https://image.tmdb.org/t/p/w500/jdjdjdjn', pitch = 'talk is cheap show me the codes')
+        self.new_feedback = Feedback(username = 'John Doe',feedback = 'I love Pomodoro Timer, helpful')
+
+    def tearDown(self):
+        Feedbacks.Clear_feedbacks()
+
+    def tes_instance(self):
+        self.assertTrue(isinstance(self.new_feedback, Feedbacks))
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_feedback.username, 'John Doe')
+        self.assertEquals(self.new_feedback, 'I love Pomodoro Timer, helpful')
+
+    def test_save_feedback(self):
+        self.new_feedback.save_feedback()
+        self.assertTrue(len(Feedbacks.query.all() > 0))
